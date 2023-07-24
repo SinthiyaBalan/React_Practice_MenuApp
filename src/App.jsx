bff0ee98d9485618a3aef85ac1c3bc4b43fa6691
SinthiyaBalan <sinthiyamct@gmail.com>
2023-07-24 17:05:54 +0200

    added tailwind CSS framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Footer from './components/Footer'
+import MainSection from './components/MainSection'
+import Nav from './components/Nav'
 
 function App() {
   
 
   return (
     <>
-      
+      <Nav/>
+      <MainSection/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainSection() {
+  return (
+    <div className='min-h-screen'>MainSection</div>
+  )
+}
+
+export default MainSection
